4a72b0b593552deceae8be270d27585442b23db8
edom <edom@edoms-air>
2026-03-11 14:26:07 -0400

    Initial Selenium QA automation project setup

---
+++ selenium-automation-project/src/main/java/com/qa/selenium_automation_project/App.java
@@ -0,0 +1,7 @@
+package com.qa.selenium_automation_project;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ selenium-automation-project/src/test/java/com/qa/selenium_automation_project/AppTest.java
@@ -0,0 +1,13 @@
+package com.qa.selenium_automation_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
